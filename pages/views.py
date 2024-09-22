9450687ad49c90b3476dfe590bff3871e5629f85
Lobanov07 <139856784+Lobanov07@users.noreply.github.com>
2024-09-22 22:48:48 +0300

    first settings

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils import timezone
+from django.db.models import Count
+from django.http import Http404
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView, CreateView, UpdateView, DeleteView, DetailView, TemplateView)
+from django.urls import reverse
 
-# Create your views here.
+# from .forms import PostForm, CommentForm
+#from .models import Post, Category, Comment
+
+
+class HomePage(TemplateView):
+    template_name = 'pages/index.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'pages/about.html'
+
+
+class RulesView(TemplateView):
+    template_name = 'pages/rules.html'
+
+
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
+
+
+def internal_server_error(request):
+    return render(request, 'pages/500.html', status=404)
+
+
+POSTS_PER_PAGE = 10
+
+
+User = get_user_model()
+
+
+
+
+#class PostListMixin(ListView):
+    #paginate_by = POSTS_PER_PAGE
+
+
